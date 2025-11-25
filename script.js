1a94bdf7d74b00562d1ef742c4581e3a6724f0ef
Beston Mobasheri <mobasheribeston@gmail.com>
2025-11-25 11:13:49 +0200

    Changed into Dom, only game-over left to implement

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 'use strict';
 
-////    1. Make a function called getComputerChoice that randonmly returns one of the following strings: rock, paper, or scissors
-
 let choices = ['rock', 'paper', 'scissors'];
 let humanScore = 0;
 let computerScore = 0;
@@ -12,65 +10,54 @@ function getComputerChoice() {
   return choices[randComp];
 }
 
-////    2. Make a function called getHumanChoice which returns one of the valid choices depending on user input(hint: Use prompt)
+let paperBtn = document.createElement('button');
+let scissorsBtn = document.createElement('button');
+let rockBtn = document.createElement('button');
+let body = document.querySelector('body');
+let divChoice = document.createElement('div');
+let divResult = document.createElement('div');
 
-function getHumanChoice() {
-  let randHuman = prompt('Choose from rock, paper, scissors!').toLowerCase();
-  console.log(`You chose: ${randHuman}`);
-  return randHuman;
-}
+paperBtn.textContent = 'Paper';
+scissorsBtn.textContent = 'Scissors';
+rockBtn.textContent = 'Rock';
+divChoice.style.border = '1px solid black';
+divResult.textContent = 'Results here';
 
-////    3. Create two variables called humanScore and comptuterScore in the global scope.
-////       Initialize those variables with the value of 0.
+body.appendChild(paperBtn);
+body.appendChild(scissorsBtn);
+body.appendChild(rockBtn);
+body.appendChild(divChoice);
+body.appendChild(divResult);
 
-////    4. Write the logic to play a single round. Create function called playRound.
-////       The two parameters in the function will be humanChoice and computerChoice
+paperBtn.addEventListener('click', () => playRound('paper'));
 
-////       The humanChoice must be case-insensitive so rock, Rock, RoCk (hint: Maybe use the toLowerCase function thingy majingy?)
+scissorsBtn.addEventListener('click', () => playRound('scissors'));
 
-////        Write the code to the functiion to console log a string value representing a winner, maybe use the `${}`??? You lose! Paper beats rock?#
+rockBtn.addEventListener('click', () => playRound('rock'));
 
-////        Lastly increment humanScore or computerScore
+function playRound(humanChoice) {
+  const computerChoice = getComputerChoice();
+  divChoice.textContent = `You chose ${humanChoice} Computer chose ${computerChoice}`;
 
-////     5. Create a function called playGame. Must move playRound into that function and make it fun 5 times
-
-function playGame() {
-  for (let i = 1; i <= 5; i++) {
-    function playRound(humanChoice, computerChoice) {
-      if (humanChoice === 'rock' && computerChoice === 'paper') {
-        console.log(`You lost this round! ${computerChoice} covers ${humanChoice}.`);
-        computerScore++;
-      } else if (humanChoice === 'paper' && computerChoice === 'rock') {
-        console.log(`Nice win! ${humanChoice} wraps ${computerChoice}.`);
-        humanScore++;
-      } else if (humanChoice === 'scissors' && computerChoice === 'paper') {
-        console.log(`You sliced your way to victory! ${humanChoice} cuts ${computerChoice}.`);
-        humanScore++;
-      } else if (humanChoice === 'paper' && computerChoice === 'scissors') {
-        console.log(`Ouch! ${computerChoice} cuts through ${humanChoice}. You lose.`);
-        computerScore++;
-      } else if (humanChoice === 'rock' && computerChoice === 'scissors') {
-        console.log(`Crushed it! ${humanChoice} smashes ${computerChoice}.`);
-        humanScore++;
-      } else if (humanChoice === 'scissors' && computerChoice === 'rock') {
-        console.log(`Defeat! ${computerChoice} crushes ${humanChoice}.`);
-        computerScore++;
-      } else {
-        console.log(`It's a tie! Both chose ${humanChoice}.`);
-      }
-    }
-
-    const humanSelection = getHumanChoice();
-    const computerSelection = getComputerChoice();
-    playRound(humanSelection, computerSelection);
+  if (humanChoice === 'rock' && computerChoice === 'paper') {
+    computerScore++;
+    divResult.textContent = `Computer won. Score: You ${humanScore} - ${computerScore} Computer`;
+  } else if (humanChoice === 'paper' && computerChoice === 'rock') {
+    humanScore++;
+    divResult.textContent = `You won! Score: You ${humanScore} - ${computerScore} Computer`;
+  } else if (humanChoice === 'scissors' && computerChoice === 'paper') {
+    humanScore++;
+    divResult.textContent = `You won! Score: You ${humanScore} - ${computerScore} Computer`;
+  } else if (humanChoice === 'paper' && computerChoice === 'scissors') {
+    computerScore++;
+    divResult.textContent = `Computer won. Score: You ${humanScore} - ${computerScore} Computer`;
+  } else if (humanChoice === 'rock' && computerChoice === 'scissors') {
+    humanScore++;
+    divResult.textContent = `You won! Score: You ${humanScore} - ${computerScore} Computer`;
+  } else if (humanChoice === 'scissors' && computerChoice === 'rock') {
+    computerScore++;
+    divResult.textContent = `Computer won. Score: You ${humanScore} - ${computerScore} Computer`;
+  } else {
+    divResult.textContent = `It's a tie! Score: You ${humanScore} - ${computerScore} Computer`;
   }
-}
-playGame();
-
-if (humanScore > computerScore) {
-  console.log(`You won: ${humanScore} VS ${computerScore}`);
-} else if (computerScore > humanScore) {
-  console.log(`You lost: ${humanScore} VS ${computerScore}`);
-} else {
-  console.log(`You've tied...${humanScore} VS ${computerScore}`);
 }
